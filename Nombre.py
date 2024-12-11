cb58efad32b8de0ba8096875cd84e3856c81c17b
i0v00 <lolguyman666@gmail.com>
2024-12-11 08:04:14 -0400

    DockerLinux

---
+++ Nombre.py
@@ -0,0 +1,10 @@
+# Solicitar información al usuario
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+fecha_nacimiento = input("Ingrese su fecha de nacimiento (dd/mm/aaaa): ")
+
+# Imprimir la información proporcionada
+print("\nInformación ingresada:")
+print(f"Nombre: {nombre}")
+print(f"Apellido: {apellido}")
+print(f"Fecha de nacimiento: {fecha_nacimiento}")
